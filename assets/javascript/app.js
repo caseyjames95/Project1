1adc149982e28b32d11ad1807797d635ba1a36d8
caseyjames95 <caseypurves@gmail.com>
2019-10-08 20:38:56 -0700

    update modals/formatting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,32 @@
-
-//  $('#authen').on('hidden.bs.modal', function () {
-//     // Load up a new modal...
-//     $('#locationModal').modal('show')
-//   })
-
+// Comment
 $('#findmyloc').click(function (e) {
+    console.log('Finding Location....')
     e.preventDefault();
-    // Coding
-    $('#locationModal').modal('hide');
+  
     getLocation();
-    // initMap();
     $('#location').modal('hide')
 
 });
 
+
+// Comment
+$('#zipbtn').click(function (e){
+    $('#location').modal('hide')
+    zipLocation();
+}
+);
+
+// Comment
 $('#newLoc').click(function (e) {
     e.preventDefault();
-    // Coding
-    $('#locationModal').modal('show');
-    
-    // initMap();
     $('#location').modal('show')
 
 });
 
+// Comment
 $('#authen').on('hidden.bs.modal', function () {
     $('#location').modal('show');
 
 });
-
-
-// document.getElementById('findmyloc').addEventListener('click', getLocation)
 
 
