3f81899b4b843f96f01d6759bad4a4820b5fba30
Francisco Raigoza <raigoza.frankie@gmail.com>
2019-10-08 21:32:09 -0700

    background fade

--- manufacturedTime/time.js
+++ manufacturedTime/time.js
@@ -2,7 +2,18 @@ $(document).ready(function(){
     $("#click").click(function(){
         $("#myGif").fadeOut(5000);
     });
-    $("#hide").click(function(){
-        $("button").hide();
-    })
+    // $("#hide").click(function(){
+    //     $("button").hide();
+    // })
+    $('button')
+.click(
+     function ()
+     {
+         $(this).hide();
+     }
+);
+$('HTML').click(function(){
+    $(this).fadeOut(5000);
+})
+
 });
