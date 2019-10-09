20385204c6b71c3789cea5bc3cc4e05effdf4db4
caseyjames95 <caseypurves@gmail.com>
2019-10-08 20:57:59 -0700

    cleanup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,15 @@
 
-//  $('#authen').on('hidden.bs.modal', function () {
-//     // Load up a new modal...
-//     $('#locationModal').modal('show')
-//   })
-
 $('#findmyloc').click(function (e) {
     console.log('hey')
     e.preventDefault();
     // Coding
-  
+
     getLocation();
     // initMap();
     $('#location').modal('hide')
 
 });
-$('#zipbtn').click(function (e){
+$('#zipbtn').click(function (e) {
     $('#location').modal('hide')
     zipLocation();
 
